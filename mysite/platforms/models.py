4527eb14be599176a844b9ee67ac346f4a148de4
NHabibahMahbub <157217743+NHabibahMahbub@users.noreply.github.com>
2024-04-23 23:26:19 +0600

    Update models.py

--- mysite/platforms/models.py
+++ mysite/platforms/models.py
@@ -10,8 +10,9 @@ class Platform(models.Model):
         ('Industrial', 'Industrial'),
         ('Special Purpose', 'Special Purpose')
     ]
-
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, default=1)
     title = models.CharField(max_length=100)
+    description = models.TextField(default="This Property is mindblowing!")
     property_type = models.CharField(max_length=20, choices=property_type_choices)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     size_sqft = models.PositiveIntegerField()
@@ -21,7 +22,20 @@ class Platform(models.Model):
     country = models.CharField(max_length=50)
     listed_date = models.DateField(auto_now_add=True)
 
+
     # image = models.ImageField()
 
     def __str__(self):
         return self.title
+
+
+class Inquiry(models.Model):
+    platform = models.ForeignKey(Platform, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    message = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Inquiry for {self.platform.title} by {self.name}"
